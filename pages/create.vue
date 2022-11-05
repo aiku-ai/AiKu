e3fab66d1442b709c2d8d9ff76d1c5179f9c70fa
danwritecode <dnelson4993@outlook.com>
2022-11-04 23:21:13 -0400

    added field level validation/error highlighting

--- pages/create.vue
+++ pages/create.vue
@@ -3,17 +3,46 @@
     <div class="lg:flex items-center justify-center">
       <div class="w-full lg:max-w-md">
         <div class="space-y-3">
-          <div class="rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-violet-600 focus-within:ring-1 focus-within:ring-violet-600 transition-hover-300">
+          <div
+            :class="lineOneValid ? 'border-gray-300 focus-within:border-violet-600 focus-within:ring-violet-600':'border-red-400 focus-within:border-red-500 focus-within:ring-red-500'"
+            class="rounded-md border px-3 py-2 shadow-sm focus-within:ring-1 transition-hover-300"
+          >
             <label for="lineOne" class="block text-xs font-medium dark:text-zinc-100 text-gray-900">Line 1: 5 syllables</label>
-            <input v-model="lineOne" type="text" name="lineOne" id="line-one" class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Hyperdrive engaged">
+            <input 
+              v-model="lineOne" 
+              type="text" 
+              name="lineOne" 
+              id="line-one" 
+              class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" 
+              placeholder="Hyperdrive engaged">
+            
           </div> 
-          <div class="rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-violet-600 focus-within:ring-1 focus-within:ring-violet-600 transition-hover-300">
+          <div 
+          :class="lineTwoValid ? 'border-gray-300 focus-within:border-violet-600 focus-within:ring-violet-600':'border-red-400 focus-within:border-red-500 focus-within:ring-red-500'"
+            class="rounded-md border px-3 py-2 shadow-sm focus-within:ring-1 transition-hover-300"
+          >
             <label for="lineTwo" class="block text-xs font-medium dark:text-zinc-100 text-gray-900">Line 2: 7 syllables</label>
-            <input v-model="lineTwo" type="text" name="lineTwo" id="line-two" class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Time and space a memory">
+            <input 
+              v-model="lineTwo" 
+              type="text" 
+              name="lineTwo" 
+              id="line-two" 
+              class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" 
+              placeholder="Time and space a memory">
           </div> 
-          <div class="rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-violet-600 focus-within:ring-1 focus-within:ring-violet-600 transition-hover-300">
+          <div
+          :class="lineThreeValid ? 'border-gray-300 focus-within:border-violet-600 focus-within:ring-violet-600':'border-red-400 focus-within:border-red-500 focus-within:ring-red-500'"
+            class="rounded-md border px-3 py-2 shadow-sm focus-within:ring-1 transition-hover-300"
+          >
             <label for="lineThree" class="block text-xs font-medium dark:text-zinc-100 text-gray-900">Line 3: 5 syllables</label>
-            <input v-model="lineThree" type="text" name="lineThree" id="line-three" class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Lightspeed traveler">
+            <input 
+              v-model="lineThree" 
+              type="text" 
+              name="lineThree" 
+              id="line-three" 
+              class="bg-zinc-100 dark:bg-zinc-800 block w-full border-0 p-0 text-zinc-800 dark:text-gray-100 placeholder-gray-500 focus:ring-0 sm:text-sm" 
+              placeholder="Lightspeed traveler"
+            >
           </div> 
         </div>
         <div class="mt-6">
@@ -141,6 +170,27 @@ const inputValid = computed(() => {
     syllable(lineTwo.value) === 7 && 
     syllable(lineThree.value) === 5
   ) {
+    return true
+  }
+  return false
+})
+
+const lineOneValid = computed(() => {
+  if(syllable(lineOne.value) === 5 || !lineOne.value) {
+    return true
+  }
+  return false
+})
+
+const lineTwoValid = computed(() => {
+  if(syllable(lineTwo.value) === 7 || !lineTwo.value) {
+    return true
+  }
+  return false
+})
+
+const lineThreeValid = computed(() => {
+  if(syllable(lineThree.value) === 5 || !lineThree.value) {
     return true
   }
   return false
