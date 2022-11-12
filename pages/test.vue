ea92aeaf407f8de9e7a45de98d6a28edb8e1ee32
danwritecode <dnelson4993@outlook.com>
2022-11-12 09:42:04 -0500

    messing mroe with canvas size

--- pages/test.vue
+++ pages/test.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="pt-40">
-    <!-- <img  -->
-    <!--   src="https://replicate.delivery/pbxt/ygtueiTM5A1HIK9YGmNsp81h2l1gRb9FEBfJXsJgzEQ9ld8PA/out-0.png"   -->
-    <!--   class="" -->
-    <!-- > -->
-    <p class="text-lg">Hello World</p>
-    <canvas id="myCanvas" class="h-96 w-96 border border-gray-200"></canvas>
+  <div class="p-10 h-screen w-full">
+    <img 
+      src="https://replicate.delivery/pbxt/ygtueiTM5A1HIK9YGmNsp81h2l1gRb9FEBfJXsJgzEQ9ld8PA/out-0.png"  
+      class="hidden"
+      id="myImage"
+    >
+    <div class="relative h-full w-full">
+      <canvas id="myCanvas" :width="width" :height="height" class="absolute inset-0 h-full w-full object-cover rounded-lg"></canvas>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const width = ref<number>(900)
+const height = ref<number>(900)
+
 if (process.client) {
-  console.log(process) 
+  // width.value = window.innerWidth
+  // height.value = window.innerHeight
+
   var c = document.getElementById("myCanvas");
-  let image = new Image("https://replicate.delivery/pbxt/ygtueiTM5A1HIK9YGmNsp81h2l1gRb9FEBfJXsJgzEQ9ld8PA/out-0.png")
+  let image = document.getElementById("myImage")
   var ctx = c.getContext("2d");
   ctx.drawImage(image,0,0)
   ctx.font = "18px Space Mono";
   ctx.fillText("Hello World", 10, 25, 300);
-}
 
+  console.log(window.innerHeight)
+  console.log(window.innerWidth)
+}
 </script>
