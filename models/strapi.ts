6ff1bf054e85add1ba3e993214f93ab6f11525ef
danwritecode <dnelson4993@outlook.com>
2022-11-13 14:06:42 -0500

    posting iamge to strapi

--- models/strapi.ts
+++ models/strapi.ts
@@ -29,3 +29,43 @@ export interface PresetPagination {
   pageCount: number;
   total:     number;
 }
+
+
+export interface ImageResponse {
+  id:                number;
+  name:              string;
+  alternativeText:   null;
+  caption:           null;
+  width:             number;
+  height:            number;
+  formats:           ImageResponseFormats;
+  hash:              string;
+  ext:               string;
+  mime:              string;
+  size:              number;
+  url:               string;
+  previewUrl:        null;
+  provider:          string;
+  provider_metadata: null;
+  createdAt:         string;
+  updatedAt:         string;
+}
+
+export interface ImageResponseFormats {
+  thumbnail: ImageResponseMedium;
+  small:     ImageResponseMedium;
+  medium:    ImageResponseMedium;
+}
+
+export interface ImageResponseMedium {
+  name:   string;
+  hash:   string;
+  ext:    string;
+  mime:   string;
+  path:   null;
+  width:  number;
+  height: number;
+  size:   number;
+  url:    string;
+}
+
