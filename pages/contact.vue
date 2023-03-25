76858658ca1eb0ac2742a4e8fb7105cdccc9ac07
danwritecode <dnelson4993@outlook.com>
2023-03-25 09:58:11 -0400

    added contact page

---
+++ pages/contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dark:bg-gray-900 bg-zinc-100 py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl space-y-16 divide-y divide-zinc-100 lg:mx-0 lg:max-w-none">
+        <div class="grid grid-cols-1 gap-y-10 gap-x-8 lg:grid-cols-3">
+          <div>
+            <h2 class="text-3xl font-bold tracking-tight dark:text-zinc-100 text-zinc-900">Get in touch</h2>
+            <p class="mt-4 leading-7 dark:text-zinc-400 text-zinc-600">Reach out on twitter or our Github</p>
+          </div>
+          <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+            <div class="rounded-2xl dark:bg-gray-800 bg-zinc-50 p-10">
+              <h3 class="text-base font-semibold leading-7 dark:text-zinc-100 text-zinc-900">To Talk</h3>
+              <dl class="mt-3 space-y-1 text-sm leading-6 text-zinc-600">
+                <div>
+                  <dt class="sr-only">Twitter</dt>
+                  <dd><a target="_blank" class="font-semibold dark:text-violet-500 text-violet-600"
+                      href="https://twitter.com/mynamebedan">twitter: @mynamebedan</a></dd>
+                </div>
+              </dl>
+            </div>
+            <div class="rounded-2xl dark:bg-gray-800 bg-zinc-50 p-10">
+              <h3 class="text-base font-semibold leading-7 dark:text-zinc-100 text-zinc-900">To Contribute</h3>
+              <dl class="mt-3 space-y-1 text-sm leading-6 text-zinc-600">
+                <div>
+                  <dt class="sr-only">Github</dt>
+                  <dd><a target="_blank" class="font-semibold dark:text-violet-500 text-violet-600"
+                      href="https://github.com/aiku-ai/AiKu/issues">Github Issues</a></dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
